030c5561fbb29df8b7e6507233a9ca8a6ed74a38
ramraj saini <ramrajsainiiitd@gmail.com>
2024-04-06 15:28:13 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,10 @@ import './../styles/App.css';
 
 const App = () => {
   const [MovieTitle, setMovieTitle] = useState('');
-  const[MovieData, setMovieData] = useState(1);
+  const[MovieData, setMovieData] = useState(null);
   const APIkey =  '99eb9fd1'
-  function MovieSearch() {
+  function MovieSearch(e) {
+    e.preventDefault();
     const APIurl = `http://www.omdbapi.com/?apikey=${APIkey}&t=${MovieTitle}`
     fetch(`http://www.omdbapi.com/?apikey=${APIkey}&t=${MovieTitle}`)
       .then(response => response.json())
@@ -19,11 +20,23 @@ const App = () => {
   }
 
   console.log(MovieData)
+if(MovieData==null){
+  return (<div>
+    <form onSubmit={MovieSearch} >
+    <label htmlFor="title">Search Movie: </label> <br/>
+    <input type="text" id="title" value={MovieTitle} onChange={(e)=>setMovieTitle(e.target.value)} />
+    <button>Search</button>
+    </form>
+  </div>
+  )
+}
 if (MovieData.Response === 'False'){
     return (<div>
-      Search Movie: <br />
-      <input type="text" value={MovieTitle} onChange={(e)=>setMovieTitle(e.target.value)} />
-      <button onClick={MovieSearch} >Search</button>
+      <form onSubmit={MovieSearch} >
+      <label htmlFor="title">Search Movie: </label> <br/>
+      <input type="text" id="title" value={MovieTitle} onChange={(e)=>setMovieTitle(e.target.value)} />
+      <button>Search</button>
+      </form>
       <p className="error">Invalid movie name. Please try again.</p>
     </div>
     )
@@ -32,12 +45,17 @@ if (MovieData.Response === 'False'){
 else{
   return(
     <div>
+      <form onSubmit={MovieSearch} >
       Search Movie: <br />
       <input type="text" value={MovieTitle} onChange={(e)=>setMovieTitle(e.target.value)} />
-      <button onClick={MovieSearch} >Search</button> <br/> <br/> <br/>
-      {MovieData.Title} <br/> <br/>
-      {MovieData.Year} <br/> <br/>
+      <button onClick={MovieSearch} >Search</button>
+      </form> <br/>
+      <ul>
+        <li>
+      {MovieData.Title} ({MovieData.Year}) <br/> <br/>
       <img src={MovieData.Poster} />
+        </li>
+      </ul>
     </div>
   )
 
