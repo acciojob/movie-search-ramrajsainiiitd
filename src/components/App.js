a9540456323d305c00fe0d6b02935caf3f09ce52
ramraj saini <ramrajsainiiitd@gmail.com>
2024-04-07 12:09:31 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
   const [MovieTitle, setMovieTitle] = useState('');
-  const[MovieData, setMovieData] = useState(null);
-  const APIkey =  '99eb9fd1'
-  function MovieSearch(e) {
+  const [MovieData, setMovieData] = useState(null);
+
+  async function MovieSearch(e) {
     e.preventDefault();
-    const APIurl = `http://www.omdbapi.com/?apikey=${APIkey}&t=${MovieTitle}`
-    fetch(`http://www.omdbapi.com/?apikey=${APIkey}&t=${MovieTitle}`)
+    await fetch(`https://www.omdbapi.com/?s=${MovieTitle}&apikey=99eb9fd1`)
       .then(response => response.json())
       .then(data => {
         setMovieData(data);
@@ -19,47 +17,48 @@ const App = () => {
       });
   }
 
-  console.log(MovieData)
-if(MovieData==null){
-  return (<div>
-    <form onSubmit={MovieSearch} >
-    <label htmlFor="title">Search Movie: </label> <br/>
-    <input type="text" id="title" value={MovieTitle} onChange={(e)=>setMovieTitle(e.target.value)} />
-    <button>Search</button>
-    </form>
-  </div>
-  )
-}
-if (MovieData.Response === 'False'){
-    return (<div>
-      <form onSubmit={MovieSearch} >
-      <label htmlFor="title">Search Movie: </label> <br/>
-      <input type="text" id="title" value={MovieTitle} onChange={(e)=>setMovieTitle(e.target.value)} />
-      <button>Search</button>
-      </form>
-      <p className="error">Invalid movie name. Please try again.</p>
-    </div>
-    )
-  
-}
-else{
-  return(
+  if (MovieData === null) {
+    return (
+      <div>
+        <form onSubmit={MovieSearch} >
+          <label htmlFor="title">Search Movie: </label> <br/>
+          <input type="text" id="title" value={MovieTitle} onChange={(e) => setMovieTitle(e.target.value)} />
+          <button type="submit" >Search</button>
+        </form>
+      </div>
+    );
+  }
+
+  if (MovieData.Response === 'False') {
+    return (
+      <div>
+        <form onSubmit={MovieSearch} >
+          <label htmlFor="title">Search Movie: </label> <br/>
+          <input type="text" id="title" value={MovieTitle} onChange={(e) => setMovieTitle(e.target.value)} />
+          <button type="submit" >Search</button>
+        </form>
+        <p className="error">Invalid movie name. Please try again.</p>
+      </div>
+    );
+  }
+
+  return (
     <div>
       <form onSubmit={MovieSearch} >
-      Search Movie: <br />
-      <input type="text" value={MovieTitle} onChange={(e)=>setMovieTitle(e.target.value)} />
-      <button onClick={MovieSearch} >Search</button>
+        Search Movie: <br />
+        <input type="text" value={MovieTitle} onChange={(e) => setMovieTitle(e.target.value)} />
+        <button type="submit" >Search</button>
       </form> <br/>
       <ul>
-        <li>
-      {MovieData.Title} ({MovieData.Year}) <br/> <br/>
-      <img src={MovieData.Poster} />
-        </li>
+        {MovieData.Search.map((item) => (
+          <li key={item.imdbID}>
+            {item.Title} ({item.Year}) <br/> <br/>
+            <img src={item.Poster} alt={`${item.Title} Poster`} />
+          </li>
+        ))}
       </ul>
     </div>
-  )
-
-}
+  );
 }
 
-export default App
+export default App;
